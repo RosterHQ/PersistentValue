75ac2d2a68275a6ecf1e79d14f8ed3ee8196a8c9
Christopher Prince <chris@SpasticMuffin.biz>
2019-10-26 19:47:22 -0600

    Unified the property wrapper syntax for basic and codable types.

--- Sources/PersistentValue/Persist.swift
+++ Sources/PersistentValue/Persist.swift
@@ -10,18 +10,84 @@ import Foundation
 // See https://nshipster.com/propertywrapper/
 @propertyWrapper
 public struct Persist<Type> {
-    private let persisted: PersistentValue<Type>
+    private var basic: PersistentValue<Type>?
+    private var codable: AnyObject?
     
+    // Got this idea from (https://stackoverflow.com/questions/38213286/overriding-methods-in-swift-extensions)
+    // because I was having a lot of problems with extensions.
+    private var getter:(() -> Type?)!
+    private var setter:((Type?) -> ())!
+    
+    // I'd like to have access to the String name of the property being wrapped, to use as a default for `name`, but haven't figured out how do that yet: See (https://stackoverflow.com/questions/58575321/access-name-of-object-being-wrapped-in-swift-property-wrapper-implementation)
+
     public init(in storage: PersistentValueStorage, name: String, initialValue: Type? = nil) {
-        persisted = try! PersistentValue<Type>(name: name, storage: storage)
+        basic = try! PersistentValue<Type>(name: name, storage: storage)
         
-        if let initialValue = initialValue, persisted.value == nil {
-            persisted.value = initialValue
+        if let initialValue = initialValue, basic!.value == nil {
+            basic!.value = initialValue
+        }
+        
+        getter = basicGetter
+        setter = basicSetter
+    }
+    
+    private func basicGetter() -> Type? {
+        if let basic = basic {
+            return basic.value
+        }
+        return nil
+    }
+    
+    private func basicSetter(object: Type?) {
+        if let basic = basic {
+            basic.value = object
         }
     }
 
+    // This gets used for both basic and codable.
     public var wrappedValue: Type? {
-        get { persisted.value }
-        set { persisted.value = newValue }
+        get {
+            return getter()
+        }
+        
+        set {
+            setter(newValue)
+        }
     }
 }
+
+// To use @Persist with Codable's, adopt not only the Codable protocol, but also adopt the Persisted protocol. (I had to use this second protocol because otherwise Int's and others are detected as Codable.)
+public protocol Persisted {
+}
+
+extension Persist where Type: Codable & Persisted {
+    public init(in storage: PersistentValueStorage, name: String, initialValue: Type? = nil) {
+        let persistentCodable = PersistentCodableValue<Type>(name: name, storage: storage)
+        codable = persistentCodable
+        
+        if let initialValue = initialValue, persistentCodable.value == nil {
+            persistentCodable.value = initialValue
+        }
+        
+        getter = codableGetter
+        setter = codableSetter
+    }
+    
+    private func codableGetter() -> Type? {
+        if let codable = codable as? PersistentCodableValue<Type> {
+            return codable.value
+        }
+        return nil
+    }
+    
+    private func codableSetter(object: Type?) {
+        if let codable = codable as? PersistentCodableValue<Type> {
+            codable.value = object
+        }
+    }
+    
+    // I would have liked to have just overridden:
+    //      public var wrappedValue: Type?
+    // but while overidding the init method above works, overriding the property does not. i.e., the overridden property wrappedValue never gets used.
+}
+
---
+++ Tests/PersistentValueTests/CodablePropertyWrapper.swift
@@ -0,0 +1,111 @@
+//
+//  CodablePropertyWrapper.swift
+//  PersistentValue_Tests
+//
+//  Created by Christopher G Prince on 10/26/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import XCTest
+@testable import PersistentValue
+
+// Testing Codable's with @Persist
+
+class CodablePropertyWrapper: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    struct CodableStruct: Equatable, Codable, Persisted {
+        let int: Int
+        let str: String
+    }
+
+    @Persist(in: .userDefaults, name: "persistCodableUserDefaultsWithNoInitialValue")
+        var codableUserDefaultsWithNoInitialValue: CodableStruct?
+    static let value1 = CodableStruct(int: 3, str: "foo2")
+    
+    @Persist(in: .userDefaults, name: "persistCodableUserDefaultsWithInitialValue", initialValue: CodablePropertyWrapper.value1)
+        var codableUserDefaultsWithInitialValue: CodableStruct
+        
+    func testCodableUserDefaultsIntWithNoInitialValue() {
+        XCTAssert(codableUserDefaultsWithNoInitialValue == nil)
+        
+        let value = CodableStruct(int: 1, str: "foo")
+        codableUserDefaultsWithNoInitialValue = value
+        
+        XCTAssert(codableUserDefaultsWithNoInitialValue == value)
+        codableUserDefaultsWithNoInitialValue = nil
+    }
+
+    func testCodableUserDefaultsIntWithInitialValue() {
+        XCTAssert(codableUserDefaultsWithInitialValue == CodablePropertyWrapper.value1)
+        
+        let value2 = CodableStruct(int: 3, str: "foo2")
+        codableUserDefaultsWithInitialValue = value2
+        
+        XCTAssert(codableUserDefaultsWithInitialValue == value2)
+        codableUserDefaultsWithInitialValue = CodablePropertyWrapper.value1
+    }
+    
+    // MARK: Codable, KeyChain
+
+    @Persist(in: .keyChain, name: "persistCodableKeyChainWithNoInitialValue")
+        var codableKeyChainWithNoInitialValue: CodableStruct?
+
+    @Persist(in: .keyChain, name: "persistCodableKeyChainWithInitialValue", initialValue: CodablePropertyWrapper.value1)
+        var codableKeyChainWithInitialValue: CodableStruct
+        
+    func testCodableKeyChainWithNoInitialValue() {
+        XCTAssert(codableKeyChainWithNoInitialValue == nil)
+        
+        let value = CodableStruct(int: 1, str: "foo")
+        codableKeyChainWithNoInitialValue = value
+        
+        XCTAssert(codableKeyChainWithNoInitialValue == value)
+        codableKeyChainWithNoInitialValue = nil
+    }
+    
+    func testCodableKeyChainWithInitialValue() {
+        XCTAssert(codableKeyChainWithInitialValue == CodablePropertyWrapper.value1)
+        
+        let value2 = CodableStruct(int: 3, str: "foo2")
+        codableKeyChainWithInitialValue = value2
+        
+        XCTAssert(codableKeyChainWithInitialValue == value2)
+        codableKeyChainWithInitialValue = CodablePropertyWrapper.value1
+    }
+    
+    // MARK: Codable, File
+
+    @Persist(in: .file, name: "persistCodableFileWithNoInitialValue")
+        var codableFileWithNoInitialValue: CodableStruct?
+
+    @Persist(in: .file, name: "persistCodableFileWithInitialValue", initialValue: CodablePropertyWrapper.value1)
+        var codableFileWithInitialValue: CodableStruct
+        
+    func testCodableFileWithNoInitialValue() {
+        XCTAssert(codableFileWithNoInitialValue == nil)
+        
+        let value = CodableStruct(int: 1, str: "foo")
+        codableFileWithNoInitialValue = value
+        
+        XCTAssert(codableFileWithNoInitialValue == value)
+        codableFileWithNoInitialValue = nil
+    }
+    
+    func testCodableFileWithInitialValue() {
+        XCTAssert(codableFileWithInitialValue == CodablePropertyWrapper.value1)
+        
+        let value2 = CodableStruct(int: 3, str: "foo2")
+        codableFileWithInitialValue = value2
+        
+        XCTAssert(codableFileWithInitialValue == value2)
+        codableFileWithInitialValue = CodablePropertyWrapper.value1
+    }
+}
